720a9df06d970afc9456558268863af0ab956307
0xidm <0xidm@protonmail.com>
2023-01-22 16:18:50 -0500

    extract addresses to stand-alone json file, hopefully github can search this one

---
+++ extract_address_column.py
@@ -0,0 +1,28 @@
+import csv
+import json
+import argparse
+
+import pandas as pd
+
+
+def extract_address_column(path):
+    df = pd.read_csv(path)
+    addresses = df['address'].tolist()
+    with open('addresses.json', 'w') as f:
+        f.write(json.dumps(addresses, indent=1))
+
+if __name__ == "__main__":
+    # Create the parser
+    my_parser = argparse.ArgumentParser(description='Extract the address column from a CSV file')
+
+    # Add the arguments
+    my_parser.add_argument('Path',
+                           metavar='path',
+                           type=str,
+                           help='the path to the CSV file')
+
+    # Execute the parse_args() method
+    args = my_parser.parse_args()
+
+    # Print the results
+    extract_address_column(args.Path)
